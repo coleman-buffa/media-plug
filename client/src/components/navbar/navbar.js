dac42431113f917adc7d8703dba15db5647ff3f5
jessny <jessnyjoseph@gmail.com>
2021-03-05 16:15:48 -0600

    added initial structure to nav

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,9 +1,123 @@
+// import React from 'react';
+// import './navbar.css';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab';
+
+// const useStyles = makeStyles({
+// 	root: {
+// 		flexGrow: 1,
+// 	},
+// });
+
+// function Navbar() {
+// 	const classes = useStyles();
+// 	const [value, setValue] = React.useState(0);
+
+// 	const handleChange = (event, newValue) => {
+// 		setValue(newValue);
+// 	};
+// 	return (
+// 		<Paper className={classes.root}>
+// 			<Tabs
+// 				value={value}
+// 				onChange={handleChange}
+// 				indicatorColor="primary"
+// 				textColor="primary"
+// 				centered
+// 			>
+// 				<Tab label="Home" />
+// 				<Tab label="Profile" />
+// 				<Tab label="Explore" />
+// 				<Tab label="Challenges" />
+// 			</Tabs>
+// 		</Paper>
+// 	);
+// }
+
+// export default Navbar;
+
 import React from 'react';
-import './navbar.css';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const useStyles = makeStyles({
+	list: {
+		width: 250,
+	},
+	fullList: {
+		width: 'auto',
+	},
+});
 
 function Navbar() {
-	return(
-		<div>Navbar!</div>
+	const classes = useStyles();
+	const [state, setState] = React.useState({
+		left: false
+	});
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+
+		setState({ ...state, [anchor]: open });
+	};
+
+	const list = (anchor) => (
+		<div
+			className={clsx(classes.list, {
+				[classes.fullList]: anchor === 'top' || anchor === 'bottom',
+			})}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				{['Profile', 'Explore', 'Challenges'].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />  }</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+			<List>
+				{['Signout'].map((text) => (
+					<ListItem button key={text}>
+						<ListItemIcon>{<ExitToAppIcon/>}</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
+	return (
+		<div>
+			{['left'].map((anchor) => (
+				<React.Fragment key={anchor}>
+					<Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+					<SwipeableDrawer
+						anchor={anchor}
+						open={state[anchor]}
+						onClose={toggleDrawer(anchor, false)}
+						onOpen={toggleDrawer(anchor, true)}
+					>
+						{list(anchor)}
+					</SwipeableDrawer>
+				</React.Fragment>
+			))}
+		</div>
 	);
 }
 
