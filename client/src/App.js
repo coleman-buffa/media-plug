b9bddd2118d4f3ff28c930b384940b0a4598ff2e
Muhammad K <akhalid.code@gmail.com>
2021-03-05 12:06:45 -0800

    add login page UI and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import UserContext from "./utils/usercontext";
@@ -16,13 +16,29 @@ import Footer from "./components/footer/footer"
 
 function App() {
   const [user, setUser] = useState({});
+
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleSignupBtnClick = e => {
+    setRegisterUsername(e.target.value);
+  }
+
+  const handleLoginBtnClick = e => {
+
+  }
+
+
   return (
     <div>
-      <UserContext.Provider value={{ username, email, authenticated, handleSignupBtnClick, handleLoginBtnClick }}>
+      <UserContext.Provider value={{ user, setUser, registerUsername, setRegisterUsername, registerPassword, setRegisterPassword, loginUsername, setLoginUsername, loginPassword, setLoginPassword, handleSignupBtnClick, handleLoginBtnClick }}>
         <Navbar />
         <Router>
           <Switch>
-            <Route exact path="/" component={Signup} />
+            <Route exact path="/" component={Login} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/explore" component={Explore} />
--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './footer.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { BottomNavigation } from '@material-ui/core';
+
+const useStyles = makeStyles({
+	foot: {
+		position: 'fixed',
+		width: "100%",
+		backgroundColor: '#3F51B5',
+		color: '#C2C8E7',
+		bottom: 0
+	}
+})
 
 function Footer() {
-	return(
-		<div>Footer!</div>
+	const classes = useStyles();
+	return (
+		<BottomNavigation className={classes.foot}>
+			<h6>Media Plug designed by Coleman Buffa, Jessny Joseph, and Muhammad A Khalid</h6>
+		</BottomNavigation>
 	);
 }
 
