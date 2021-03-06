71a4ef4fa8db34dcfcfb7cfc1a015afda6fb2398
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-06 12:24:17 -0800

    Update UserChallenge model associations.

--- models/UserChallenge.js
+++ models/UserChallenge.js
@@ -2,7 +2,7 @@ module.exports = function (sequelize, DataTypes) {
   let UserChallenge = sequelize.define("UserChallenge", {    
   });
   UserChallenge.associate = models => {
-    models.UserChallenge.hasMany(models.User, {as: "participant"});
+    models.UserChallenge.belongsTo(models.User, {as: "participant"});
     models.UserChallenge.belongsTo(models.Challenge);
   }
   return UserChallenge;
