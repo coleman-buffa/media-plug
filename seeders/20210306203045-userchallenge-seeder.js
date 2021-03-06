2a5284797a2b67f0ab9646f8f013a4c0fc1adc8d
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-06 12:47:11 -0800

    Create database seeders.

--- seeders/20210306203045-userchallenge-seeder.js
+++ seeders/20210306203045-userchallenge-seeder.js
@@ -3,19 +3,17 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 
-     await queryInterface.bulkInsert('userbooks', [{
-       read_status: false,
-       UserId: 1,
-       BookId: 1,
-       createdAt: new Date(),
-       updatedAt: new Date()
-     },
-    {
-      read_status: true,
-      UserId: 1,
-      BookId: 2,
+    await queryInterface.bulkInsert('userchallenges', [{
       createdAt: new Date(),
-      updatedAt: new Date() 
+      updatedAt: new Date(),
+      participantId: 1,
+      ChallengeId: 1
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      participantId: 1,
+      ChallengeId: 2
     }]);
 
   },
