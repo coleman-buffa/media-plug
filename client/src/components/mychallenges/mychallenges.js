de2adbe5c774ab91c74ca929edfa2675e313bbf2
jessny <jessnyjoseph@gmail.com>
2021-03-06 22:08:39 -0600

    made add challenge icon into button

--- client/src/components/mychallenges/mychallenges.js
+++ client/src/components/mychallenges/mychallenges.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import ChallengeCard from "../challengecard/challengecard";
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+import IconButton from '@material-ui/core/IconButton';
 import "./mychallenges.css";
 
 function MyChallenges() {
@@ -12,7 +13,10 @@ function MyChallenges() {
             <CssBaseline />
             <Container style={{width:"95%", marginTop: 30}}>       
                 <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh', padding: 30, paddingTop: 20, fontSize:30 }}>
-                My Challenges <PlaylistAddIcon className="playlistadd"/>
+                My Challenges 
+                <IconButton className="playlistadd" color="primary" aria-label="add new challenge">
+                <PlaylistAddIcon />
+                </IconButton>
                 <ChallengeCard />   
                     </Typography>       
             </Container>
