aee2dceeffd520fa11514a0a7e95bf2bc11ada51
jessny <jessnyjoseph@gmail.com>
2021-03-08 13:53:30 -0600

    cleaned up code in bookshelf

--- client/src/components/bookshelf/bookshelf.js
+++ client/src/components/bookshelf/bookshelf.js
@@ -2,19 +2,8 @@ import React from 'react';
 import "./bookshelf.css";
 import { makeStyles, Card, CardMedia, CardContent, Box } from '@material-ui/core';
 // import { Rating } from '@material-ui/lab';
-// import Grid from '@material-ui/core/Grid';
-
 
 const useStyles = makeStyles({
-    userCard: {
-        width: 220,
-        height: 270,
-        marginLeft: 25,
-    },
-    shelfContent: {
-        // display: 'flex',
-        // justifyContent: 'space-evenly',
-    },
     shelfImage: {
         height: 260,
         width: 190,
@@ -31,36 +20,21 @@ const useStyles = makeStyles({
 });
 
 function BookShelf(props) {
-    // const [value, setValue] = React.useState(2);
     const classes = useStyles();
     return (
         <div>
-            {/* <Grid container spacing={3}>
-                <Grid item xs={6} sm={3}> */}
-                    {/* < Card className={classes.userCard} elevation={24}>
-                <CardContent className={classes.shelfContent}> */}
-                    <CardMedia
-                        className={classes.shelfImage}
-                        image={props.image}
-                        title="Book Cover"
-                        
-                    />
-                    {/* <Box component="fieldset" borderColor="transparent">
+            <CardMedia
+                className={classes.shelfImage}
+                image={props.image}
+                title="Book Cover"
+
+            />
+            {/* <Box component="fieldset" borderColor="transparent">
                         <Rating className={classes.rating} name="read-only" value={props.rating} readOnly />
-                    </Box> */}
-                    {/* </CardContent>
-            </Card > */}
-                {/* </Grid>
-            </Grid> */}
+                    </Box>  */}
         </div>
     );
 }
 
 export default BookShelf;
 
-{/* <div className="container">
-        <div className="booksRead"><img src={props.image} /></div>
-        <div className="shelf"></div>
-</div> */}
-
-{/* <div className="shelf"></div> */ }
