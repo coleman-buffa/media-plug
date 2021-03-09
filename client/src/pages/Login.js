bda2b580c96fce05a51d3f674d10bd9f6bcceaa2
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-09 11:50:07 -0800

    Add Auth0 login and logout buttons that displayed based on the user's login status.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,53 +4,49 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const useStyles = makeStyles({
-	pagecont: {
-		alignContent: "center"
-	},
-	cardcont: {
-		marginTop: "10%",
-		padding: 10,
-		display: "flex",
-		justifyContent: "center"
-	},
-	formcont: {
-		alignContent: "center",
-		margin: '0 50px',
-	},
-	inputcont: {
-		margin: 5
-	}
+  pagecont: {
+    alignContent: "center"
+  },
+  cardcont: {
+    marginTop: "10%",
+    padding: 10,
+    display: "flex",
+    justifyContent: "center"
+  },
+  formcont: {
+    alignContent: "center",
+    margin: '0 50px',
+  },
+  inputcont: {
+    margin: 5
+  }
 });
 
 function Login() {
-	const classes = useStyles();
-	const { loginWithRedirect, logout } = useAuth0();
 
-	return (
-		<Container className={classes.pagecont} md="true">
-			<Card className={classes.cardcont}>
-				<form>
-					<FormControl className={classes.formcont}>
-						<TextField required id="reg-user" label="Required" placeholder="Username" variant="outlined" className={classes.inputcont} />
-						<TextField required id="reg-email" label="Required" placeholder="Email" variant="outlined" className={classes.inputcont} />
-						<TextField id="reg-pass" label="Password" autoComplete="current-password" type="password" variant="outlined" className={classes.inputcont} />
-						<Button variant="contained" color="primary">Signup</Button>
-					</FormControl>
-				</form>
-				<form>
-					<FormControl className={classes.formcont}>
-						<TextField required id="login-user" label="Required" placeholder="Username" variant="outlined" className={classes.inputcont} />
-						<TextField required id="login-email" label="Required" placeholder="Email" variant="outlined" className={classes.inputcont} />
-						<TextField id="login-pass" label="Password" autoComplete="current-password" type="password" variant="outlined" className={classes.inputcont} />
-						{/* All we need below! */}
-						<Button variant="contained" color="primary" onClick={() => loginWithRedirect()}>Login</Button>
-						<Button variant="contained" color="primary" onClick={() => logout()}>Logout</Button>
-					</FormControl>
-				</form>
+  const classes = useStyles();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+  // const {email} = user;
 
-			</Card>
-		</Container>
-	);
+  return (
+    <Container className={classes.pagecont} md="true">
+      <Card className={classes.cardcont}>   
+        <form>
+          <FormControl className={classes.formcont}>   
+            {/* All we need below! */}           
+            
+            {isAuthenticated ?          
+            (<Button variant="contained" color="primary" onClick={() => logout()}>Logout</Button>):(<Button variant="contained" color="primary" onClick={() => loginWithRedirect()}>Login</Button>)}
+          </FormControl>
+        </form>
+        <div>
+          {/* User email: {email} */}
+        </div>
+
+
+      </Card>
+    </Container>
+  );
 }
 
 export default Login;
